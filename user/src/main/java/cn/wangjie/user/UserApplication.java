a0eb6d9114107955296cba8df089f01003e49eab
wangjie <wjie@33.cn>
2018-08-05 20:31:05 +0800

    一些改动

--- user/src/main/java/cn/wangjie/user/UserApplication.java
+++ user/src/main/java/cn/wangjie/user/UserApplication.java
@@ -1,20 +1,39 @@
 package cn.wangjie.user;
 
-import org.springframework.boot.SpringApplication;
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
+
+
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableHystrix
+@EnableCircuitBreaker
 public class UserApplication {
     @Bean
+    @LoadBalanced
     public RestTemplate restTemplate(){
         return new RestTemplate();
     }
+    @Bean
+    public ServletRegistrationBean getServlet() {
+        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+        registrationBean.setLoadOnStartup(1);
+        registrationBean.addUrlMappings("/hystrix.stream");
+        registrationBean.setName("HystrixMetricsStreamServlet");
+        return registrationBean;
+    }
 
-    public static void main(String[] args) {
-        SpringApplication.run(UserApplication.class, args);
+        public static void main(String[] args) {
+        new SpringApplicationBuilder(UserApplication.class).web(true).run(args);
     }
 }
