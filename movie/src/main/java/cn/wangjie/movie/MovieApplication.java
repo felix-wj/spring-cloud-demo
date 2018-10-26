60d5ac961edc484e9a864da24aaf1d59554ac6c3
wangjie <wjie@33.cn>
2018-10-26 20:07:41 +0800

    使用ribbin将通过ip:port的url调用接口的形式改为通过应用名调用

--- movie/src/main/java/cn/wangjie/movie/MovieApplication.java
+++ movie/src/main/java/cn/wangjie/movie/MovieApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
@@ -15,6 +16,7 @@ import org.springframework.web.client.RestTemplate;
 public class MovieApplication {
 
     @Bean
+    @LoadBalanced
     public RestTemplate restTemplate(){
         return new RestTemplate();
     }
--- movie/src/main/java/cn/wangjie/movie/controller/UserController.java
+++ movie/src/main/java/cn/wangjie/movie/controller/UserController.java
@@ -2,8 +2,11 @@ package cn.wangjie.movie.controller;
 
 
 import cn.wangjie.movie.entity.User;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -13,14 +16,16 @@ import org.springframework.web.client.RestTemplate;
  * @author: WangJie
  * @create: 2018-07-08 14:08
  **/
+@Slf4j
 @RestController
 public class UserController {
 
     @Autowired
     private RestTemplate restTemplate;
-    @GetMapping("/user/get")
-    public User getUser(){
-        return  this.restTemplate.getForObject("http://localhost:20001/user/get",User.class);
+    @GetMapping("/user/get/{id}")
+    public User getUser(@PathVariable("id") Integer id){
+        log.info("get user");
+        return  this.restTemplate.getForObject("http://spring-cloud-user/user/get/"+id,User.class);
     }
 
 }
