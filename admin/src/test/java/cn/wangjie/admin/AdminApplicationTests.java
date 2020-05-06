e1d56d2cc44f306f78b605a508793ff87b3808b3
派大星 <1358726405@qq.com>
2020-05-06 16:03:27 +0800

    admin

---
+++ admin/src/test/java/cn/wangjie/admin/AdminApplicationTests.java
@@ -0,0 +1,14 @@
+package cn.wangjie.admin;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
