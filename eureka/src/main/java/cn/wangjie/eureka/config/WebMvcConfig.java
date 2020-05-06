11f23dc6a1ddcd9417c9aff66f2ba4edd42daa2c
派大星 <1358726405@qq.com>
2020-05-06 14:58:19 +0800

    admin

---
+++ eureka/src/main/java/cn/wangjie/eureka/config/WebMvcConfig.java
@@ -0,0 +1,41 @@
+package cn.wangjie.eureka.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.web.context.request.async.TimeoutCallableProcessingInterceptor;
+import org.springframework.web.servlet.config.annotation.AsyncSupportConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @program: demo
+ * @description:
+ * @author: WangJie
+ * @create: 2020-05-06 14:20
+ **/
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void configureAsyncSupport(final AsyncSupportConfigurer configurer) {
+        configurer.setDefaultTimeout(60 * 1000L);
+        configurer.registerCallableInterceptors(timeoutInterceptor());
+        configurer.setTaskExecutor(threadPoolTaskExecutor());
+    }
+
+    @Bean
+    public TimeoutCallableProcessingInterceptor timeoutInterceptor() {
+        return new TimeoutCallableProcessingInterceptor();
+    }
+
+    @Bean
+    public ThreadPoolTaskExecutor threadPoolTaskExecutor() {
+        ThreadPoolTaskExecutor t = new ThreadPoolTaskExecutor();
+        t.setCorePoolSize(10);
+        t.setMaxPoolSize(100);
+        t.setQueueCapacity(20);
+        t.setThreadNamePrefix("WYF-Thread-");
+        return t;
+    }
+
+}
