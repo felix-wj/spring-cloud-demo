70ff03917223f339363aae6ef6b0ae16002d70b2
派大星 <1358726405@qq.com>
2020-05-06 13:38:01 +0800

    admin

--- eureka/src/main/java/cn/wangjie/eureka/EurekaApplication.java
+++ eureka/src/main/java/cn/wangjie/eureka/EurekaApplication.java
@@ -1,9 +1,11 @@
 package cn.wangjie.eureka;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableAdminServer
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaApplication {
